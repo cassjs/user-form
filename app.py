bb49f0aa2c5f96aa4c1270fb64df4ea305027d3c
cassjsdev <cassjes.m@gmail.com>
2021-08-23 23:58:42 -0700

    added basic routing urls for register/login/dash

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/register")
+def register():
     return render_template('register.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/dashboard")
+def dashboard():
+    # Temporary Placeholder
+    return "<p>You're looking at the Dashboard.</p>"
