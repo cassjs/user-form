4ba96e952a6087c4ab10ef59f714cc79a0bf1408
cassjsdev <cassjes.m@gmail.com>
2021-08-25 00:57:47 -0700

    removed old notes

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-# from flask import Flask, render_template, url_for
-
-# app = Flask(__name__)
-
-# @app.route("/register")
-# def register():
-#     return render_template('register.html')
-
-# @app.route("/login")
-# def login():
-#     return render_template('login.html')
-
-# @app.route("/dashboard")
-# def dashboard():
-#     # Temporary Placeholder
-#     return "<p>This is a placeholder. You're looking at the Dashboard.</p>"
-
-# @app.route("/userprofile")
-# def userprofile():
-#     # Temporary Placeholder
-#     return "<p>This is a placeholder. You're looking at your profile.</p>"
-
 import os
 from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, session, url_for
